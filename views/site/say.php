9652d6c278d04f01f3e74ae40b7c4782757a7180
Alexey Shelikhov <rackvy@gmail.com>
2019-01-11 01:17:57 +1000

    yii2 install and quick setup

---
+++ views/site/say.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexey
+ * Date: 11/01/2019
+ * Time: 01:07
+ */
+use yii\helpers\Html;
+?>
+<h1>Hello <?= Html::encode($target) ?></h1>
+<p>Welcome to your Yii2 demonstration application.</p>
